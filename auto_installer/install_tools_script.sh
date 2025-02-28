42889d0f5822b0c5e3d79da122fea455786acee2
Mario <marioalvarezfdz@gmail.com>
2025-02-28 11:37:53 +0100

    Update install_tools_script.sh

--- auto_installer/install_tools_script.sh
+++ auto_installer/install_tools_script.sh
@@ -4,7 +4,7 @@ apt update && apt upgrade -y
 
 # Instalar dependencias necesarias
 echo "Instalando dependencias..."
-apt install -y curl git nmap net-tools golang sqlmap curl flatpak gobuster docker.io hydra wget
+apt install -y curl git nmap net-tools golang sqlmap curl flatpak gobuster docker.io hydra wget wfuzz john wireshark arp-scan
 
 wget https://github.com/brannondorsey/naive-hashcat/releases/download/data/rockyou.txt
 
