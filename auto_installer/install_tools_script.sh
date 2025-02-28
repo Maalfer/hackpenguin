1f1dbaf72d171e436b04a88f6647cd2c287bae16
Mario <marioalvarezfdz@gmail.com>
2025-02-28 10:43:06 +0100

    Update install_tools_script.sh

--- auto_installer/install_tools_script.sh
+++ auto_installer/install_tools_script.sh
@@ -4,10 +4,10 @@ apt update && apt upgrade -y
 
 # Instalar dependencias necesarias
 echo "Instalando dependencias..."
-apt install -y curl git nmap net-tools golang subfinder sqlmap nuclei
+apt install -y curl git nmap net-tools golang subfinder sqlmap nuclei sqlmap
 
-echo 'export GOPATH=$HOME/go' >> ~/.zshrc
-echo 'export PATH=$PATH:$GOPATH/bin' >> ~/.zshrc
+echo 'export GOPATH=$HOME/go' >> ~/.bashrc
+echo 'export PATH=$PATH:$GOPATH/bin' >> ~/.bashrc
 
 mkdir /opt/nuclei-templates
 
@@ -17,12 +17,18 @@ mv nuclei-templates/* /opt/nuclei-templates
 
 rm -r nuclei-templates
 
-go install github.com/projectdiscovery/httpx/cmd/httpx@latest
-go install github.com/projectdiscovery/katana/cmd/katana@latest
-go install github.com/tomnomnom/waybackurls@latest
-go install -v github.com/projectdiscovery/urlfinder/cmd/urlfinder@latest
-go install -v github.com/tomnomnom/anew@latest
+go install github.com/projectdiscovery/httpx/cmd/httpx@latest && \
+go install github.com/projectdiscovery/katana/cmd/katana@latest && \
+go install github.com/tomnomnom/waybackurls@latest && \
+go install -v github.com/projectdiscovery/urlfinder/cmd/urlfinder@latest && \
+go install -v github.com/tomnomnom/anew@latest && \
+go install github.com/lc/gau/v2/cmd/gau@latest
+
+echo 'subfinder -d example.com -silent | gau | katana -silent | waybackurls | grep -Ei "confidential|secret|bak|api|key|auth|token|password|config|credential"' >> /opt/bug_bounty_dorks.txt
+echo 'subfinder -d example.com -silent | gau --subs | grep -Ei "(\?|&)(q|search|id|name|query|redirect|url)=[^&]*" | httpx -silent -status-code -content-type | grep "200" | sort -u' >> /opt/bug_bounty_dorks.txt
+echo 'katana -u https://example.com/ -d 5 -jc | grep "\.js$" | tee alljs.txt'  >> /opt/finding_aws_buckets.txt
+echo 'cat alljs.txt | xargs -I {} curl -s {} | grep -oE "http[s]?://[^"]*.s3.amaxonaws.com" | sort -u' >> /opt/finding_aws_buckets.txt
 
 apt autoremove -y
 
-source ~/.zshrc
+source ~/.bashrc
